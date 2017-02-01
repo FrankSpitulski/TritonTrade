334b809f1fb4052e4900f263e00fa482c12f9dc7
Nikolas Nikravesh <nikolasn97@gmail.com>
2017-02-01 00:53:26 -0800

    Added Forgot Password Activity

--- app/src/main/java/com/tmnt/tritontrade/MainActivity.java
+++ app/src/main/java/com/tmnt/tritontrade/MainActivity.java
@@ -1,7 +1,9 @@
 package com.tmnt.tritontrade;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,4 +14,12 @@ public class MainActivity extends AppCompatActivity {
 
         Server.test(this);
     }
+
+    public void sendToForgotActivity(View view) {
+
+        Intent intent = new Intent(this, ForgotPassword.class);
+        startActivity(intent);
+
+    }
+
 }
