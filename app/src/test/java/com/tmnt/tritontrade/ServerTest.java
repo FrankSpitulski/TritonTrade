383c37282a0ed0a558dee3e337f883d05f0690a4
Kevin <k5mao@ucsd.edu>
2017-02-03 15:20:31 -0800

    Partially Implemented testUserJsonConversion

--- app/src/test/java/com/tmnt/tritontrade/ServerTest.java
+++ app/src/test/java/com/tmnt/tritontrade/ServerTest.java
@@ -2,9 +2,11 @@ package com.tmnt.tritontrade;
 
 import org.junit.Test;
 import java.util.ArrayList;
-
 import static org.junit.Assert.*;
-import static com.tmnt.tritontrade.Server.*;
+import java.lang.reflect.*;
+
+import com.tmnt.tritontrade.Server;
+
 
 /**
  * Unit tests for Server class
@@ -17,6 +19,8 @@ public class ServerTest {
     @Test
     public void testUserJsonConversion()
     {
+        //TODO FIX, CAN NOT getClass() ON GENERICS SUCH AS ARRAY LIST<USER> FOR REFLECTION
+
         //user to convert to JSON
         User user1 = new User(
                 "NAME", "PHOTOURL", 12345, "MY LIFE ARE INTERESTING", "(555) 555-5555",
@@ -27,13 +31,48 @@ public class ServerTest {
         ArrayList<User> list = new ArrayList<User>();
         list.add(user1);
 
-        //convert to json and back
-       String json = userToJson(list);
-        ArrayList<User> list2 = jsonToUser(json);
+
+        //new server instance to call invoke methods
+        Server server = new Server();
+
+        //Method to invoke
+        Method userToJson = null;
+        Method jsonToUser = null;
+
+        //declarations
+        ArrayList<User> list2 = null;
+        String json = null;
+
+        Class[] userJsonClass = {list2.getClass()};
+        Class[] jsonUserClass = {json.getClass()};
+        //get methods from class
+        try {
+            userToJson = Server.class.getDeclaredMethod("userToJson",userJsonClass);
+            jsonToUser = Server.class.getDeclaredMethod("jsonToUser",jsonUserClass);
+        } catch (NoSuchMethodException e) {
+            //fail();
+            e.printStackTrace();
+        }
+
+
+
+        //set private methods are accessible
+        userToJson.setAccessible(true);
+        jsonToUser.setAccessible(true);
+
+        //try to convert to json and back
+        try {
+            //convert to json and back
+            json = (String)userToJson.invoke(server,list);
+           list2 = (ArrayList<User>) jsonToUser.invoke(server,json);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
 
         //compare to strings that are the same
         assertTrue(list2.get(0).toString().equals(list.get(0).toString()));
-
     }
 
     /**
