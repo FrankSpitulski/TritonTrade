90d9208a844b29290b37ded21a753c7c37740d8f
edwardj9 <edwardj525@gmail.com>
2017-02-09 21:54:43 -0800

    Removed fab

--- app/src/main/java/com/tmnt/tritontrade/Mainfeed.java
+++ app/src/main/java/com/tmnt/tritontrade/Mainfeed.java
@@ -22,15 +22,7 @@ public class Mainfeed extends AppCompatActivity
         setContentView(R.layout.activity_mainfeed);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+        
 
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
