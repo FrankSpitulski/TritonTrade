4049dcf5f37c496d8e9b4dd32f9170925c9b63d1
edwardj9 <edwardj525@gmail.com>
2017-02-09 19:34:20 -0800

    Added required activities

---
+++ app/src/main/java/com/tmnt/tritontrade/Create_Post.java
@@ -0,0 +1,13 @@
+package com.tmnt.tritontrade;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Create_Post extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create__post);
+    }
+}
