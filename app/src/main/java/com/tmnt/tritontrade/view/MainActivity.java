4fd990e85cd76269ffa2d82a692882c32ddbce3b
Omar Khawaja <omarkhwj@gmail.com>
2017-02-22 10:20:49 -0800

    removed test button from main

--- app/src/main/java/com/tmnt/tritontrade/view/MainActivity.java
+++ app/src/main/java/com/tmnt/tritontrade/view/MainActivity.java
@@ -18,15 +18,6 @@ public class MainActivity extends AppCompatActivity {
 
         //Server.test(this);
 
-        Button test = (Button) findViewById(R.id.poptest);
-        test.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent t = new Intent(MainActivity.this, PopUpPost.class);
-                startActivity(t);
-            }
-        });
-
     }
 
     //If the user clicks on the "Forgot Password?" button, they will be redirected to
