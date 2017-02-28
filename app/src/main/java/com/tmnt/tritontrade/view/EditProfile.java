298cc46e162a6f7e9b1d840f09e99b4515f8ca0e
Nikolas Nikravesh <nikolasn97@gmail.com>
2017-02-27 17:42:36 -0800

    Implemented Edit Profile functionality (except change profile photo), havent linked up to database yet either

--- app/src/main/java/com/tmnt/tritontrade/view/EditProfile.java
+++ app/src/main/java/com/tmnt/tritontrade/view/EditProfile.java
@@ -1,15 +1,84 @@
 package com.tmnt.tritontrade.view;
 
+import android.app.Activity;
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 import com.tmnt.tritontrade.R;
+import com.tmnt.tritontrade.controller.DownloadPhotosAsyncTask;
+import com.tmnt.tritontrade.controller.Server;
+import com.tmnt.tritontrade.controller.User;
 
 public class EditProfile extends AppCompatActivity {
+
+    private User currUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_profile);
+
+
+        currUser = getIntent().getParcelableExtra("user");
+
+        populateFields(currUser);
+
+    }
+
+
+    private void populateFields(User user){
+
+        //Get references to all the relevant fields in the profle
+        EditText username = (EditText) findViewById(R.id.username);
+        EditText phone = (EditText) findViewById(R.id.phone);
+        EditText bio = (EditText) findViewById(R.id.bio);
+
+        //set the Edit text fields
+        username.setText(user.getName());
+        phone.setText(user.getMobileNumber());
+        bio.setText(user.getBio());
+
+        //set profile photo
+        new DownloadPhotosAsyncTask((ImageView) findViewById(R.id.profile_image))
+                .execute(user.getPhoto());
+
+    }
+
+    public void cancelEdit(View view){
+        finish();
+    }
+
+    public void saveInfo(View view) {
+        //Get references to all the relevant fields in the profle
+        EditText username = (EditText) findViewById(R.id.username);
+        EditText phone = (EditText) findViewById(R.id.phone);
+        EditText bio = (EditText) findViewById(R.id.bio);
+
+
+        if(username.getText().toString().matches("")) {
+            //error
+        } else {
+            currUser.setName(username.getText().toString());
+        }
+
+
+        if(phone.getText().toString().matches("")) {
+            //error
+        } else {
+            currUser.setMobileNumber(phone.getText().toString());
+        }
+
+        currUser.setBio(bio.getText().toString());
+
+
+        Intent resultIntent = new Intent();
+        resultIntent.putExtra("updatedUser", currUser);
+        setResult(Activity.RESULT_OK, resultIntent);
+        finish();
     }
 }
--- app/src/main/java/com/tmnt/tritontrade/view/Profile.java
+++ app/src/main/java/com/tmnt/tritontrade/view/Profile.java
@@ -1,5 +1,7 @@
 package com.tmnt.tritontrade.view;
+import android.app.Activity;
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -7,6 +9,10 @@ import android.widget.ImageView;
 import android.widget.ListView;
 import android.widget.TextView;
 
+import com.google.android.gms.appindexing.Action;
+import com.google.android.gms.appindexing.AppIndex;
+import com.google.android.gms.appindexing.Thing;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.tmnt.tritontrade.R;
 import com.tmnt.tritontrade.controller.Post;
 import com.tmnt.tritontrade.controller.User;
@@ -15,40 +21,66 @@ import com.tmnt.tritontrade.controller.DownloadPhotosAsyncTask;
 public class Profile extends AppCompatActivity {
 
     private ListView list;
+    private int EDIT_PROFILE = 1;
+
+    private User currUser;
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    private GoogleApiClient client;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_profile);
 
-        User user = new User("User", "http://farm7.staticflickr.com/6047/7036787275_951cb768fe.jpg", 0,
+        currUser = new User("User", "http://farm7.staticflickr.com/6047/7036787275_951cb768fe.jpg", 0,
                 "This is the bio for the user", "(510) 696-6969", "email@lol.edu",
                 "jkdsf", "sdfs", null, false, null, "sdfs", false);
 
         new DownloadPhotosAsyncTask((ImageView) findViewById(R.id.userPic))
-                .execute(user.getPhoto());
+                .execute(currUser.getPhoto());
 
-        populateUserInfo(user);
+        populateUserInfo(currUser);
         populateList();
 
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
     }
 
 
     public void sendToEditProfile(View view) {
 
         Intent intent = new Intent(this, EditProfile.class);
-        startActivity(intent);
+
+        intent.putExtra("user", currUser);
+
+        startActivityForResult(intent, EDIT_PROFILE);
 
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
 
-    private void populateUserInfo(User user){
+        if(requestCode == EDIT_PROFILE){
+            if(resultCode == Activity.RESULT_OK) {
+                currUser = data.getParcelableExtra("updatedUser");
+                populateUserInfo(currUser);
+            }
+        }
+    }
+
+
+    private void populateUserInfo(User user) {
 
         //Get references to all the relevant fields in the profle
         TextView username = (TextView) findViewById(R.id.username);
         TextView email = (TextView) findViewById(R.id.email);
         TextView phone = (TextView) findViewById(R.id.phone);
-        TextView bio = (TextView) findViewById(R.id.description);
+        TextView bio = (TextView) findViewById(R.id.bio);
 
         //populate the fields with the information obtained from the user
         username.setText(user.getName());
@@ -66,10 +98,10 @@ public class Profile extends AppCompatActivity {
         //random posts
         Post[] posts = new Post[10];
 
-        for(int i = 0; i < posts.length; i++){
+        for (int i = 0; i < posts.length; i++) {
 
 
-            Post p = new Post("Product Title " + i , null, "Description of product. This is going to probably be a multiple line " +
+            Post p = new Post("Product Title " + i, null, "Description of product. This is going to probably be a multiple line " +
                     "description. What were to happen if I added more lines to this shit? " +
                     "Description # " + i, 69, null, 0, 0, false, false, null, "hi", false);
 
@@ -82,5 +114,40 @@ public class Profile extends AppCompatActivity {
         //update the list
         list.setAdapter(new ProfileListAdaptor(this, posts));
 
+    }
+
+    /**
+     * ATTENTION: This was auto-generated to implement the App Indexing API.
+     * See https://g.co/AppIndexing/AndroidStudio for more information.
+     */
+    public Action getIndexApiAction() {
+        Thing object = new Thing.Builder()
+                .setName("Profile Page")
+                .setUrl(Uri.parse("http://[ENTER-YOUR-URL-HERE]"))
+                .build();
+        return new Action.Builder(Action.TYPE_VIEW)
+                .setObject(object)
+                .setActionStatus(Action.STATUS_TYPE_COMPLETED)
+                .build();
+    }
+
+    @Override
+    public void onStart() {
+        super.onStart();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        client.connect();
+        AppIndex.AppIndexApi.start(client, getIndexApiAction());
+    }
+
+    @Override
+    public void onStop() {
+        super.onStop();
+
+        // ATTENTION: This was auto-generated to implement the App Indexing API.
+        // See https://g.co/AppIndexing/AndroidStudio for more information.
+        AppIndex.AppIndexApi.end(client, getIndexApiAction());
+        client.disconnect();
     }
 }
