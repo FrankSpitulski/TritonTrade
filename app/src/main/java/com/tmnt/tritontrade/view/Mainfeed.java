42079aabf0eb57e3ae2c12e47e9f9983c23ab1f1
Nikolas Nikravesh <nikolasn97@gmail.com>
2017-03-02 13:20:00 -0800

    Transition to Settings from Mainfeed

--- app/src/main/java/com/tmnt/tritontrade/view/Mainfeed.java
+++ app/src/main/java/com/tmnt/tritontrade/view/Mainfeed.java
@@ -147,6 +147,8 @@ public class Mainfeed extends AppCompatActivity
 
         //noinspection SimplifiableIfStatement
         if (id == R.id.action_settings) {
+            Intent intent = new Intent(this, Settings.class);
+            startActivity(intent);
             return true;
         }
 
