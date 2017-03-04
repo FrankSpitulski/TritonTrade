b3e345f910c05080f08ce2de25749c77c7ec7a55
bernardwang2 <bernw7214604@gmail.com>
2017-03-04 03:27:19 -0800

    licenses

---
+++ app/src/main/java/com/tmnt/tritontrade/view/License_Displays.java
@@ -0,0 +1,15 @@
+package com.tmnt.tritontrade.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.tmnt.tritontrade.R;
+
+public class License_Displays extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_license__displays);
+    }
+}
