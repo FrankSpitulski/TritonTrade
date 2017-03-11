966064e89fdcfe638ee0b4b4db55b90e652b8b65
Kevin <k5mao@ucsd.edu>
2017-03-11 11:29:45 -0800

    Added Tests

--- app/src/androidTest/java/com/tmnt/tritontrade/InstrumentedMobileNumberConversionTest.java
+++ app/src/androidTest/java/com/tmnt/tritontrade/InstrumentedMobileNumberConversionTest.java
@@ -2,6 +2,7 @@ package com.tmnt.tritontrade;
 
 
 import android.support.test.runner.AndroidJUnit4;
+import android.util.Log;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -20,11 +21,14 @@ import static org.junit.Assert.*;
 public class InstrumentedMobileNumberConversionTest
 {
     //paired array lists that store the unformatted number and the expected result of each
-    ArrayList<String> unformattedNumbers;
-    ArrayList<String> databaseFormatNumbers;
+    static ArrayList<String> unformattedNumbers;
+    static ArrayList<String> databaseFormatNumbers;
 
+    /**
+     * Initializes the array lists holding the test cases
+     */
     @BeforeClass
-    public void setup()
+    public static void setup()
     {
         //initialize the lists
         unformattedNumbers = new ArrayList<String>();
@@ -32,8 +36,36 @@ public class InstrumentedMobileNumberConversionTest
 
         //add elements to the lists
 
+        //standard database format numbers
+        unformattedNumbers.add("+0001 (123) 532-1111");
+        databaseFormatNumbers.add("+0001 (123) 532-1111");
+
+        //10 digits no spaces
+        unformattedNumbers.add("6382431324");
+        databaseFormatNumbers.add("+0001 (638) 243-1324");
+
+        unformattedNumbers.add("0000000000");
+        databaseFormatNumbers.add("+0001 (000) 000-0000");
+
+        //no country code
+        unformattedNumbers.add("(632) 888-7777");
+        databaseFormatNumbers.add("+0001 (632) 888-7777");
+
+        unformattedNumbers.add("(000) 000-0000");
+        databaseFormatNumbers.add("+0001 (000) 000-0000");
+
+        //10 digits, space between are and number, dash in number
+        unformattedNumbers.add("134 432-2222");
+        databaseFormatNumbers.add("+0001 (134) 432-2222");
+
+        //dashes between are and number
+        unformattedNumbers.add("838-999-9999");
+        databaseFormatNumbers.add("+0001 (838) 999-9999");
     }
 
+    /**
+     * Tests whether or not numbers are converted to the database format properly
+     */
     @Test
     public void testConvertMobileNumberToDatabaseFormat()
     {
@@ -45,6 +77,7 @@ public class InstrumentedMobileNumberConversionTest
 
             if (result == null)
             {
+                Log.d("DEBUG","Phone Number \"" + unformattedNumbers.get(x) + "\" failed");
                 //fail, valid number could not be converted
                 fail();
             }
