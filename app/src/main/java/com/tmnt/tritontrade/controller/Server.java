14502b1b8525268b09468b474d417d73bd53711a
Kevin <k5mao@ucsd.edu>
2017-03-11 12:12:34 -0800

    Fixed io to illegal argument exception

--- app/src/main/java/com/tmnt/tritontrade/controller/Server.java
+++ app/src/main/java/com/tmnt/tritontrade/controller/Server.java
@@ -188,7 +188,7 @@ public class Server {
 
         String regexMobile = "^\\+[0-9][0-9][0-9][0-9] \\([0-9][0-9][0-9]\\) [0-9][0-9][0-9]-[0-9][0-9][0-9][0-9]$";
         if(mobileNumber==null || !mobileNumber.matches(regexMobile))
-            throw new IOException("Phone number does not match acceptable pattern.");
+            throw new IllegalArgumentException("Phone number does not match acceptable pattern.");
 
         // check to see if email is already registered
         String response = httpGetRequest("/db/api.php/users?filter[]=email,eq,"
