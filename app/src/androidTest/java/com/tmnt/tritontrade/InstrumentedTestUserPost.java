0f98bd59a8606924820550a5490e7b96436e20a5
Kevin <k5mao@ucsd.edu>
2017-03-14 13:41:52 -0700

    Minor formatting on tests

---
+++ app/src/androidTest/java/com/tmnt/tritontrade/InstrumentedTestUserPost.java
@@ -0,0 +1,33 @@
+package com.tmnt.tritontrade;
+
+import android.support.test.runner.AndroidJUnit4;
+
+import com.tmnt.tritontrade.controller.User;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+
+import java.util.ArrayList;
+
+import static junit.framework.Assert.assertEquals;
+import static junit.framework.Assert.assertTrue;
+
+/**
+ * Tests Post and User data and methods without server interaction
+ */
+@RunWith(AndroidJUnit4.class)
+public class InstrumentedTestUserPost {
+
+
+    @Test
+    public void testPostHistory()
+    {
+        User u = new User("Name",User.getDefaultImage(),1,"BIO","+0001 (510) 888-8888",
+                "bob@ucsd.edu","hunter2","SASLLLLLT",new ArrayList<Integer>(),true,
+                new ArrayList<Integer>(),"Asd",false);
+
+        u.addToPostHistory(100);
+
+        assertTrue(100 == u.getPostHistory().get(0));
+    }
+}
